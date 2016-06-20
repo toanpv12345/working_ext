56e70c0f9f539e34dd7653ee089f70d6c37532ee
Toanpv <phamviettoan1986@gmail.com>
2016-06-20 08:43:03 +0700

    add sulution

---
+++ TestCS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using VETC;
+
+namespace TestCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encriptKey = "FC9DC415668CF44B";
+            string serverIP = "192.168.70.56";
+            string account = "test01";
+            string pwd = "test@123";
+            int station = 1;
+            int serverPort = 2699;
+
+            VETCService.ServiceEvent += new VETCServiceEvent(OnReceiver);
+
+            VETCService service = new VETCService(serverIP, serverPort, account, pwd, station, encriptKey);
+
+            Console.Read();
+        }
+
+        static void OnReceiver(string packet)
+        {
+            Console.WriteLine(packet);
+        }
+    }
+}
